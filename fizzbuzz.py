9870aa7723974368a453ce4f59496317152a841e
leeroyRoot <leeroywilliams64@gmail.com>
2019-05-15 16:09:16 +0200

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,19 @@
+"""
+	@Author:	Leeroy P. Williams
+	@Date:		15/05/2019
+	@Description:	Should display Fizz for multiples of 3 and
+			Buzz for multiples of 5, then display FizzBuzz
+			for multiples of both
+"""
+
+x = [x for x in range(1,101)]
+
+for i in x:
+	if (i%3 == 0) and (i%5 == 0):
+		print("FizzBuzz")
+	elif i%3 == 0:
+		print("Fizz")
+	elif i%5 == 0:
+		print("Buzz")
+	else:
+		print(i)
